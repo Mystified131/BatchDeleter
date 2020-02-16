7071f3d3c53c97d756652f90b3f496a2933e4a1a
mystified131 <mystifiedthomas@gmail.com>
2020-02-16 08:17:07 -0600

    Initial commit

---
+++ BatchDeleter.py
@@ -0,0 +1,19 @@
+#This code imports the necessary modules
+
+import os
+import re
+import collections
+import operator
+ 
+for subdir, dirs, files in os.walk('C:\\Users\\mysti\\thomasoriginalcode\\Git\\'):
+    for file in files:
+        filepath = subdir + os.sep + file
+
+        if (filepath.endswith(".wav")) :
+            os. remove(filepath) 
+
+print("")
+
+print("WAV and MP3 files have been removed. Thank you.")
+
+print("")
